036104780ddd5defd585c8c42d4c949435e367bf
Braulio Vargas López <braulio.vargas@glovoapp.com>
2022-07-15 02:52:28 +0200

    Create new project: service

---
+++ platform/service.tf
@@ -0,0 +1,45 @@
+module "service" {
+  source                  = "../../modules/github-repository"
+  name                    = "service"
+  description             = "TestRepo"
+  default_branch          = "main"
+  team                   = "platform" 
+  project                = "service"
+
+  team_collaborators = [
+    {
+      team_id = data.github_team.platform.id,
+      permission = "admin"
+    }
+    ,
+    {
+      team_id    = data.github_team.platform.id,
+      permission = "push"
+    },
+    {
+      team_id = data.github_team.livedb-stakeholders.id,
+      permission = "pull"
+    }
+  ]
+  branch_protections = [
+    {
+      pattern                = "main"
+      enforce_admins         = false
+      require_signed_commits = false
+      required_status_checks = {
+        strict   = true
+        contexts = [
+          "Terraform Cloud/glovo/service-prod",
+          "Terraform Cloud/glovo/service-stage",
+          "continuous-integration/jenkins/pr-merge"
+        ]
+      }
+      required_pull_request_reviews = {
+        dismiss_stale_reviews           = true
+        require_code_owner_reviews      = true
+        required_approving_review_count = 2
+      }
+    }
+  ]
+  webhooks = [local.jenkins_webhook, local.spinnaker_webhook]
+}
